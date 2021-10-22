181ec1427860903cfa3c0983d262c060b79fd40e
Miguel Valencia <valencimm@gmail.com>
2021-10-22 19:22:49 +1000

    Color detection

--- color_detector.py
+++ color_detector.py
@@ -1,48 +1,163 @@
 import cv2
 import numpy as np
 import os
+from math import *
+from numpy.core.fromnumeric import argmin
+from ximea import xiapi
 
-RED     = 0
-GREEN   = 1
-BLUE    = 2
-YELLOW  = 3
+from color_picker import YELLOW
+'''
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+'''
 
-def bgr2hsv(bgr):
-    return cv2.cvtColor(np.array([[bgr]]), cv2.COLOR_BGR2HSV)[0][0]
+MOUSE_LEFT = 1
+class ColorDetector:
 
-def hsv2bgr(hsv):
-    return cv2.cvtColor(np.array([[hsv]]), cv2.COLOR_HSV2BGR)[0][0]
+    """
+    Color detection class:
+    Detects RGBY as well as White (W) and Black (K)
+    """
 
-current_dir = os.getcwd()
-file = open(current_dir + "/colors.config", 'r')
-i = 0
-bgr_red = None
-bgr_green = None
-bgr_blue = None
-bgr_yellow = None
-for line in file:
-    entries = line.split(' ')[1:]
-    values = [int(x) for x in entries]
-    if i == RED:
-        bgr_red = np.array([values[0], values[1], values[2]]).astype(np.uint8)
-        hsv_red = bgr2hsv(bgr_red)
-    if i == GREEN:
-        bgr_green = np.array([values[0], values[1], values[2]]).astype(np.uint8)
-        hsv_green = bgr2hsv(bgr_green)
-    if i == BLUE:
-        bgr_blue = np.array([values[0], values[1], values[2]]).astype(np.uint8)
-        hsv_blue = bgr2hsv(bgr_blue)
-    if i == YELLOW:
-        bgr_yellow = np.array([values[0], values[1], values[2]]).astype(np.uint8)
-        hsv_yellow = bgr2hsv(bgr_yellow)
-    i += 1
+    RED     = 0
+    GREEN   = 1
+    BLUE    = 2
+    YELLOW  = 3
+    WHITE   = 4
+    BLACK   = 5
 
-print(bgr_red)
-print(bgr_green)
-print(bgr_blue)
-print(bgr_yellow)
+    def __init__(self, bgr_r, bgr_g, bgr_b, bgr_y):
+        self.bgr_r = bgr_r
+        self.bgr_g = bgr_g
+        self.bgr_b = bgr_b
+        self.bgr_y = bgr_y
 
-print(hsv_red)
-print(hsv_green)
-print(hsv_blue)
-print(hsv_yellow)
+        # Convert to hsv color space (H = Hue, S = Saturation, V = Value)
+        self.hsv_r = ColorDetector.bgr2hsv(self.bgr_r)
+        self.hsv_g = ColorDetector.bgr2hsv(self.bgr_g)
+        self.hsv_b = ColorDetector.bgr2hsv(self.bgr_b)
+        self.hsv_y = ColorDetector.bgr2hsv(self.bgr_y)
+
+    @classmethod
+    def bgr2hsv(cls, bgr):
+        return cv2.cvtColor(np.array([[bgr]]), cv2.COLOR_BGR2HSV)[0][0]
+
+    @classmethod
+    def hsv2bgr(cls, hsv):
+        return cv2.cvtColor(np.array([[hsv]]), cv2.COLOR_HSV2BGR)[0][0]
+
+    @classmethod
+    def hsv2coord(cls, hsv):
+        """
+        Maps to a cylinder with max radius = 1, height = 1
+        This representation is singularity free compared to the HSV space
+        """
+        # Hue is normalised from 0 to 2 pi
+        h = 2 * pi * hsv[0] / 180
+        # Saturation is normalised from zero to 
+        s = hsv[1] / 255
+        v = hsv[2] / 255
+        return np.array([s * v * cos(h), s * v * sin(h), v])
+
+    def detect_color(self, bgr):
+        """
+        Color detection implementation
+        Maps the HSV space as a cylinder, and finds the cartesian distance
+        The closest distance color to the test color is the color returned.
+        Black and white are also added to this color detection
+        """
+        hsv = ColorDetector.bgr2hsv(bgr)
+        coord_test = ColorDetector.hsv2coord(hsv)
+        coord_r = ColorDetector.hsv2coord(self.hsv_r)
+        coord_g = ColorDetector.hsv2coord(self.hsv_g)
+        coord_b = ColorDetector.hsv2coord(self.hsv_b)
+        coord_y = ColorDetector.hsv2coord(self.hsv_y)
+        coord_w = ColorDetector.hsv2coord(np.array([0, 0, 255]))
+        coord_k = ColorDetector.hsv2coord(np.array([0, 0, 0]))
+        
+        dist_r = np.linalg.norm(coord_test - coord_r)
+        dist_g = np.linalg.norm(coord_test - coord_g)
+        dist_b = np.linalg.norm(coord_test - coord_b)
+        dist_y = np.linalg.norm(coord_test - coord_y)
+        dist_w = np.linalg.norm(coord_test - coord_w)
+        dist_k = np.linalg.norm(coord_test - coord_k)
+
+        return argmin([dist_r, dist_g, dist_b, dist_y, dist_w, dist_k])
+
+def mouse_callback(event, x, y, flags, params):
+    """
+    Callback function when the image is left-clicked
+    """
+    image, color_detector = params
+    if event == MOUSE_LEFT:
+        bgr = image[y, x, :].astype(np.uint8)
+        # Print out the color detected
+        print("Detected " + ["red", "green", "blue", "yellow", "white", "black"][color_detector.detect_color(bgr)] + ".")
+
+
+
+def demo():
+    """
+    This is demo code for color detection.
+    You will need to modify this for your own implemention to use ROS.
+    It is suggested to use a subscriber/publisher node to listen for Images, and publish the color detected.
+    """
+    # Read the colors.config file from each line and set the color arrays
+    current_dir = os.getcwd()
+    file = open(current_dir + "/colors.config", 'r')
+    i = 0
+    for line in file:
+        entries = line.split(' ')
+        values = [int(x) for x in entries]
+        if i == 0:
+            bgr_r = np.array([values[0], values[1], values[2]]).astype(np.uint8)
+        if i == 1:
+            bgr_g = np.array([values[0], values[1], values[2]]).astype(np.uint8)
+        if i == 2:
+            bgr_b = np.array([values[0], values[1], values[2]]).astype(np.uint8)
+        if i == 3:
+            bgr_y = np.array([values[0], values[1], values[2]]).astype(np.uint8)
+        i += 1
+
+    # Initialise the color detector
+    color_detector = ColorDetector(bgr_r, bgr_g, bgr_b, bgr_y)
+
+    # Setup the XIMEA Camera
+    # For ROS, replace this with numpy array obtained from Image message
+    cam = xiapi.Camera()
+    cam.open_device()
+    cam.set_exposure(10000)
+    cam.set_imgdataformat('XI_RGB24')
+    cam.disable_auto_wb()
+    cam.start_acquisition()
+    cam_img = xiapi.Image()
+    
+    # Setup image as empty numpy array
+    image = np.zeros((cam.get_height(), cam.get_width(), 3), dtype=np.uint8)
+
+    # Create a window named Image
+    cv2.namedWindow('Image')
+    cv2.setWindowTitle('Image', "XIMEA Camera View (press q to exit)")
+    # Initialise the callback function
+    cv2.setMouseCallback('Image', mouse_callback, (image, color_detector))
+
+    # Main loop for image acquisition
+    while True:
+        try:
+            cam.get_image(cam_img)
+            img_data = cam_img.get_image_data_numpy()
+            np.copyto(image, img_data)
+            cv2.imshow("Image", image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except KeyboardInterrupt:
+            break
+    
+    print("Stopping acquisition...")
+    cam.stop_acquisition()
+    print("Closing all windows...")
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    demo()
+    
--- color_picker.py
+++ color_picker.py
@@ -7,6 +7,8 @@ MOUSE_LEFT = 1
 import cv2
 import numpy as np
 import os
+import ximea
+from ximea import xiapi
 
 RED     = 0
 GREEN   = 1
@@ -25,7 +27,7 @@ def nothing(x):
     pass
 
 def bgr2hsv(bgr):
-    return cv2.cvtColor(np.array([[bgr]]), cv2.COLOR_BGR2HSV)[0][0]
+    return cv2.cvtColor(np.array([[bgr.astype(np.uint8)]]), cv2.COLOR_BGR2HSV)[0][0]
 
 def hsv2bgr(hsv):
     return cv2.cvtColor(np.array([[hsv]]), cv2.COLOR_HSV2BGR)[0][0]
@@ -68,14 +70,24 @@ palette = np.ones((1000, 800, 3), np.uint8)
 cv2.namedWindow('palette')
 cv2.namedWindow('frame')
 
-cap = cv2.VideoCapture(0)
 cv2.setMouseCallback('frame', mouse_callback_frame, None)
 cv2.setMouseCallback('palette', mouse_callback_palette, None)
+
+#cap = cv2.VideoCapture(0)
+cam = xiapi.Camera()
+cam.open_device()
+cam.set_exposure(10000)
+cam_img = xiapi.Image()
+cam.set_imgdataformat('XI_RGB24')
+cam.disable_auto_wb()
+cam.start_acquisition()
 image = None
 
-while(1):
-    ret, frame = cap.read()
-    image = frame.copy()
+while True:
+    #ret, frame = cap.read()
+    cam.get_image(cam_img)
+    img_data = cam_img.get_image_data_numpy()
+    image = np.copy(img_data)
     palette[0:400, 0:400] = bgr_red
     palette[0:400, 400:800] = bgr_green
     palette[400:800, 0:400] = bgr_blue
@@ -89,23 +101,39 @@ while(1):
         palette = cv2.rectangle(palette, (10, 410), (390, 790), [128, 128, 128], 19)
     elif quadrant == YELLOW:
         palette = cv2.rectangle(palette, (410, 410), (790, 790), [128, 128, 128], 19)
-    palette = cv2.putText(palette, "R", (130, 260), cv2.FONT_HERSHEY_SIMPLEX, 7, [0, 0, 0], 30)
-    palette = cv2.putText(palette, "G", (530, 260), cv2.FONT_HERSHEY_SIMPLEX, 7, [0, 0, 0], 30)
-    palette = cv2.putText(palette, "B", (130, 660), cv2.FONT_HERSHEY_SIMPLEX, 7, [0, 0, 0], 30)
-    palette = cv2.putText(palette, "Y", (540, 660), cv2.FONT_HERSHEY_SIMPLEX, 7, [0, 0, 0], 30)
-    palette = cv2.putText(palette, "R", (130, 260), cv2.FONT_HERSHEY_SIMPLEX, 7, [255, 255, 255], 10)
-    palette = cv2.putText(palette, "G", (530, 260), cv2.FONT_HERSHEY_SIMPLEX, 7, [255, 255, 255], 10)
-    palette = cv2.putText(palette, "B", (130, 660), cv2.FONT_HERSHEY_SIMPLEX, 7, [255, 255, 255], 10)
-    palette = cv2.putText(palette, "Y", (540, 660), cv2.FONT_HERSHEY_SIMPLEX, 7, [255, 255, 255], 10)
+    palette = cv2.putText(palette, "R", (130 + 30, 260 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [0, 0, 0], 30)
+    palette = cv2.putText(palette, "G", (530 + 30, 260 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [0, 0, 0], 30)
+    palette = cv2.putText(palette, "B", (130 + 30, 660 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [0, 0, 0], 30)
+    palette = cv2.putText(palette, "Y", (540 + 30, 660 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [0, 0, 0], 30)
+    palette = cv2.putText(palette, "R", (130 + 30, 260 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [255, 255, 255], 10)
+    palette = cv2.putText(palette, "G", (530 + 30, 260 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [255, 255, 255], 10)
+    palette = cv2.putText(palette, "B", (130 + 30, 660 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [255, 255, 255], 10)
+    palette = cv2.putText(palette, "Y", (540 + 30, 660 - 100), cv2.FONT_HERSHEY_SIMPLEX, 3, [255, 255, 255], 10)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_red), (60, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_red), (60, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_green), (460, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_green), (460, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_blue), (60, 700), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_blue), (60, 700), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_yellow), (460, 700), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "BGR: " + str(bgr_yellow), (460, 700), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_red)), (60, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_red)), (60, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_green)), (460, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_green)), (460, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_blue)), (60, 650), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_blue)), (60, 650), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_yellow)), (460, 650), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [0, 0, 0], 12)
+    palette = cv2.putText(palette, "HSV: " + str(bgr2hsv(bgr_yellow)), (460, 650), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [255, 255, 255], 4)
     palette = cv2.putText(palette, "SAVE", (250, 940), cv2.FONT_HERSHEY_SIMPLEX, 4, [255, 255, 255], 8)
     if save:
         try:
             current_dir = os.getcwd()
             file = open(current_dir + "/colors.config", 'w+')
-            file.write("r: {} {} {}\n".format(bgr_red[0], bgr_red[1], bgr_red[2]))
-            file.write("g: {} {} {}\n".format(bgr_green[0], bgr_green[1], bgr_green[2]))
-            file.write("b: {} {} {}\n".format(bgr_blue[0], bgr_blue[1], bgr_blue[2]))
-            file.write("y: {} {} {}\n".format(bgr_yellow[0], bgr_yellow[1], bgr_yellow[2]))
+            file.write("{} {} {}\n".format(bgr_red[0], bgr_red[1], bgr_red[2]))
+            file.write("{} {} {}\n".format(bgr_green[0], bgr_green[1], bgr_green[2]))
+            file.write("{} {} {}\n".format(bgr_blue[0], bgr_blue[1], bgr_blue[2]))
+            file.write("{} {} {}\n".format(bgr_yellow[0], bgr_yellow[1], bgr_yellow[2]))
             print("Saving color calibration files to {}/colors.config".format(current_dir))
             file.close()
             file = open(current_dir + "/colors.config", 'r')
@@ -114,9 +142,10 @@ while(1):
             save = False
         except Exception as e:
             print(e)
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', img_data)
     cv2.imshow('palette', palette)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+cam.stop_acquisition()
 cv2.destroyAllWindows()
